c786856dea014885dc0fcd9ce690cac2cf12591b
Nick Rudolfsky <nickr@smule.com>
2013-11-08 11:29:47 -0800

    Adding cocos to NSRunLoopCommonModes so it runs while scrolling table

--- cocos2d/Platforms/iOS/CCDirectorIOS.m
+++ cocos2d/Platforms/iOS/CCDirectorIOS.m
@@ -478,7 +478,7 @@ CGFloat	__ccContentScaleFactor = 1;
 
 #else
 	// setup DisplayLink in main thread
-	[displayLink_ addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+	[displayLink_ addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
 #endif
 
     isAnimating_ = YES;
@@ -537,7 +537,7 @@ CGFloat	__ccContentScaleFactor = 1;
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-	[displayLink_ addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+	[displayLink_ addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
 
 	// start the run loop
 	[[NSRunLoop currentRunLoop] run];
